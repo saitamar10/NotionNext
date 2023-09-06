bb252223f443e8822d1e82580aaf63399c115382
saitamar10 <94067189+saitamar10@users.noreply.github.com>
2023-09-06 15:51:12 +0300

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,38 +5,37 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: 'Sitemize Hoşgeldiniz', url: '#' },
+    { title: 'Sitemize Hoşgeldiniz ', url: '#' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: 'SAITAMA',
+  HERO_TITLE_2: 'SENSEI',
+  HERO_TITLE_3: 'saitama.net.tr',
+  HERO_TITLE_4: 'saitama sensei',
+  HERO_TITLE_5: 'Tema Güncelleniyor',
+  HERO_TITLE_LINK: 'https://saitama.net.tr',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: 'Test', url: '/tag/test' },
+  HERO_CATEGORY_2: { title: 'Test', url: '/tag/test' },
+  HERO_CATEGORY_3: { title: 'Test', url: '/tag/test' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HERO_RECOMMEND_POST_TAG: '推荐',
+  HERO_RECOMMEND_POST_TAG: 'Saitama',
   HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    'Merhaba ben ',
+    '🔍 Araştırmayı Severim',
+    '🤝 Dostluk kurmayı severim',
+    '🏃 Sporu Severim',
+    '🏠 Düzenli bir hayat hayalim',
+    '🤖️ Yapay zekayı severim',
   ],
-  INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  INFO_CARD_URL: 'https://www.ismetceber.com.tr', // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
